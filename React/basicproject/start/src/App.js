7f5b109954d7dc1caf1290d3b427d61de5ec10d1
LiamDes <deshaiesash@gmail.com>
2023-06-14 20:38:29 -0700

    basic react project with layout

---
+++ React/basicproject/start/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <>
+    <header>
+      hi
+    </header>
+    <aside>
+      side aaaaee
+    </aside>
+    <main>
+      main
+    </main>
+    <footer>
+      footer
+    </footer>
+    </>
+  )
+}
+
+export default App;
